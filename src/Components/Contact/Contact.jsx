e4d581257aaa4434d42b7c965c244671de5c36d8
Md.Solaiman <md.solaiman366882@gmail.com>
2023-12-12 16:58:34 +0600

    react hot toast added and email send feature added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,30 @@
+import { useRef } from "react";
 import contactImg from "../../assets/images/contact.png";
+import emailjs from '@emailjs/browser';
+import toast from "react-hot-toast";
+
+
+
 
 const Contact = () => {
+    
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_2n5olqa', 'template_s542nzl', form.current, '_VkrsAdOJ0Oh4Iadl')
+        .then((result) => {
+            if(result.status == 200)
+            {
+                toast.success('Message Sent Successfully');
+            }
+        }, (error) => {
+            //console.log(error.text);
+            toast.success(`${error.text}`);
+        });
+    }
+
 	return (
 		<div className="section-padding w-full">
 			<div className="px-5">
@@ -17,15 +41,16 @@ const Contact = () => {
 				<div className="flex-1">
 					<img src={contactImg} alt="" />
 				</div>
-				<form action="" className="flex-1">
+				<form action="" className="flex-1" ref={form} onSubmit={sendEmail}>
 					<div className="form-area">
 						<div className="single-input">
 							<label htmlFor="name">Name</label>
 							<input
 								type="text"
-								name="name"
+								name="from_name"
 								placeholder="Your Name"
 								id="name"
+
 							/>
 						</div>
 						<div className="single-input">
@@ -33,7 +58,7 @@ const Contact = () => {
 							<input
 								type="email"
 								placeholder="Your Email"
-								name="email"
+								name="from_email"
 								id="email"
 							/>
 						</div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
 import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <RouterProvider router={router}></RouterProvider>
+   <Toaster />
   </React.StrictMode>,
 )
